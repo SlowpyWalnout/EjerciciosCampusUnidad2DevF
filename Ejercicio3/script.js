87959b8cc14f8570beb0a8092efb76c010bb0bb6
José Quintana <76418657+SlowpyWalnout@users.noreply.github.com>
2023-10-29 15:23:14 -0600

    Ejercicio 3

---
+++ Ejercicio3/script.js
@@ -0,0 +1,78 @@
+
+//Ejercicio 1
+function multiploDeCinco(){
+    const numero = parseInt(prompt('ingresa un numero'));
+    for(let i = 0; i <= numero; i++){
+        if(i % 5 == 0){
+            console.log(`el numero ${i} es multiplo de 5`);
+        }
+    }
+};
+//Ejercicio 2
+function loteria(){
+    const numero1 = parseInt(prompt('Ingresa un numero entre 1 y 50!'));
+    const numero2 = parseInt(prompt('Ingresa un numero entre 1 y 50!'));
+    for(let i = 0; i <= 50; i++){
+    
+        if(numero1 == i || numero2 == i){
+            console.log(`${i} loteria!`)
+            continue;
+        }
+        console.log(i)
+    }
+}
+//Ejercicio 3
+function almacenaNumeros(){
+    let array = [];
+    while(true){
+        let numero = prompt('Ingresa un numero: \nSi deseas finalizar, ingresa el 0.');
+        if(isNaN(numero) == false){
+            if(numero == 0){
+                break;
+            }else{
+                array.push(parseInt(numero));
+            }
+        }else{
+            console.log('ingresa un numero!')
+        }
+    }
+    console.log('los numeros que ingresaste: ', array)
+}
+//Ejercicio 4
+function guardarPalabras(){
+    let array = [];
+    while(true){
+        let respuesta = prompt('Ingresa una letra o una palabra: \nSi deseas terminar de ingresar letras, no escribas nada');
+        if(respuesta === ''){
+            break;
+        }else{
+            array.push(respuesta);
+        }
+    }
+    console.log('esto es lo que ingresaste...', array)
+}
+//Ejercicio 5
+function diaSemana(){
+    while(true){
+        let dia = prompt('ingresa un dia de la semana').toLowerCase();
+        if (dia == 'lunes'){
+            alert('Empezamos a chambear');
+        }else if(dia == 'martes'){
+            alert('Ya mero mitad de semana');
+        }else if(dia == 'miercoles'){
+            alert('Ya es mitad de semana');
+        }else if(dia == 'jueves'){
+            alert('Ya mero es viernes');
+        }else if(dia == 'viernes'){
+            alert('ya es viernes!');
+        }else if(dia == 'sabado'){
+            alert('empezo el fin de semana!')
+        }else if(dia == 'domingo'){
+            alert('ve a descansar');
+            break;
+        }else{
+            alert('escribe bien el dia...')
+        }
+    }
+}
+diaSemana()
